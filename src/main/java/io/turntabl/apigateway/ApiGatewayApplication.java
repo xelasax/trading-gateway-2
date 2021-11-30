28094d6969e2ca40779ab3726b0b1fd194aa782c
Quame Work PC <quamelexiz@gmail.com>
2021-11-30 20:42:29 +0000

    Updated stuff

---
+++ src/main/java/io/turntabl/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,24 @@
+package io.turntabl.apigateway;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import reactor.netty.http.client.HttpClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableWebSecurity
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
+}
---
+++ src/main/java/io/turntabl/apigateway/config/WebSecurity.java
@@ -0,0 +1,24 @@
+package io.turntabl.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@Configuration
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
+    }
+}
