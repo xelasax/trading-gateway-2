8e65bb63c44dadb581cabbf9d2af7cb25d0aa2c3
Quame <quamelexiz@gmail.com>
2021-12-07 16:06:28 +0000

    Updated Gateway Configs

--- src/main/java/io/turntabl/apigateway/ApiGatewayApplication.java
+++ src/main/java/io/turntabl/apigateway/ApiGatewayApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableWebSecurity
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
