11e651ad34e11afe8b3f86041a28751308043896
Quame <quamelexiz@gmail.com>
2021-12-07 14:32:23 +0000

    Updated gateway with spring security cors fixes

--- src/main/java/io/turntabl/apigateway/config/SpringSecurityAuth.java
+++ src/main/java/io/turntabl/apigateway/config/SpringSecurityAuth.java
@@ -1,24 +1,38 @@
 package io.turntabl.apigateway.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @Configuration
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+public class SpringSecurityAuth extends WebSecurityConfigurerAdapter {
+
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
 
-        http.authorizeRequests()
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest()
                 .authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .oauth2ResourceServer()
                 .jwt()
                 .jwtAuthenticationConverter(jwtAuthenticationConverter);
+
+        http.headers().frameOptions().disable();
     }
 }
